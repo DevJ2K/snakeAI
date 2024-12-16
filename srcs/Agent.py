99a0aa5ecc5846f54267c7a466a99fd0545be914
theo <ajavontheopro@gmail.com>
2024-12-16 17:57:57 +0100

    💭 : Can import a model and display board next vision...

--- srcs/Agent.py
+++ srcs/Agent.py
@@ -3,11 +3,15 @@ import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from srcs.Snake import Snake
 import utils.Colors as Colors
+import json
 
 class Agent(Snake):
-    def __init__(self, size: int = 10, snake_length: int = 3) -> None:
+    def __init__(self, size: int = 10, snake_length: int = 3, model_file: str = None) -> None:
         super().__init__(size, snake_length)
-        pass
+        self.model = self.get_model(model_file)
+
+    def __str__(self) -> str:
+        return str(self.model)
 
     def vision(self) -> list[list[str]]:
         head_i = self.snake[0].i
@@ -42,17 +46,55 @@ class Agent(Snake):
                 display_str += "\n"
         print(display_str)
 
+    def display_board_and_vision(self):
+        v_board = self.vision() # Vision board
+        g_board = self.board # Game board
 
+        display_str = ""
+        for i in range(len(g_board)):
+            for j in range(len(g_board[i])):
+                item = self.get_item_by_char(g_board[i][j])
+                if item is not None:
+                    display_str += item['color'] + "  " + Colors.RESET
+                else:
+                    display_str += Colors.YELLOWHB + "  " + Colors.RESET
+
+            display_str += "  "
+            for j in range(len(v_board[i])):
+                item = self.get_item_by_char(v_board[i][j])
+                if item is not None:
+                    display_str += item['color'] + "  " + Colors.RESET
+                else:
+                    display_str += Colors.YELLOWHB + "  " + Colors.RESET
+
+            display_str += "\n"
+        print(display_str)
+
+    def get_model(self, file: str = None) -> dict:
+        if file:
+            with open(file) as fd:
+                return json.load(fd)
+                # print(self.model)
+        else:
+           return {
+                "session": 0,
+                "max_sessions": 100,
+                "max_length": 0,
+                "max_duration": 0.0,
+                "q_table": {}
+            }
 
 
 
 if __name__ == "__main__":
     # from pprint import pprint
 
-    agent = Agent(size=10, snake_length=3)
+    agent = Agent(size=10, snake_length=3, model_file="models/10sess.json")
+    agent.display_board_and_vision()
+    print(agent)
     # snake._Snake__place_random_apple({})
-    agent.display_board(False)
-    agent.display_vision(False)
+    # agent.display_board(False)
+    # agent.display_vision(False)
     # time.sleep(2)
     # direction_list = {
     #     "U": agent.UP,
